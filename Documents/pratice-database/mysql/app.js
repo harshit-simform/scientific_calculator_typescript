d37c3775ca62d12c260f55127e6f77eea58f8db7
harshit-simform <harshit.g@simformsolutions.com>
2023-06-06 16:52:46 +0530

    feat: completed all feauture

--- Documents/pratice-database/mysql/app.js
+++ Documents/pratice-database/mysql/app.js
@@ -1,5 +1,17 @@
 import express from "express";
-import { getAllUser, createUser, getUser, deleteUser } from "./database.js";
+import {
+  getAllUser,
+  createUser,
+  getUser,
+  deleteUser,
+  getAllUndeliverOrder,
+  fiveRecentOrder,
+  topActiveUser,
+  inActiveUser,
+  mostPurchasedProduct,
+  mostExpensiveOrder,
+  leastExpensiveOrder,
+} from "./database.js";
 const app = express();
 import productRouter from "./product/productController.js";
 import orderRouter from "./orders/orderController.js";
@@ -63,7 +75,118 @@ app.delete("/user/:id", async (req, res, next) => {
   }
 });
 
+app.get("/undeliver-orders", async (req, res) => {
+  try {
+    const result = await getAllUndeliverOrder();
+    res.status(200).json({
+      status: "success",
+      totalOrder: result[0].length,
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+
+app.get("/most-recent-orders", async (req, res) => {
+  try {
+    const result = await fiveRecentOrder();
+    res.status(200).json({
+      status: "success",
+      totalOrder: result[0].length,
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+app.get("/top-active-user", async (req, res) => {
+  try {
+    const result = await topActiveUser();
+    res.status(200).json({
+      status: "success",
+      totalUser: result[0].length,
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+
+app.get("/inactive-user", async (req, res) => {
+  try {
+    const result = await inActiveUser();
+    res.status(200).json({
+      status: "success",
+      totalUser: result[0].length,
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+
+app.get("/most-purchased-product", async (req, res) => {
+  try {
+    const result = await mostPurchasedProduct();
+    res.status(200).json({
+      status: "success",
+      totalProduct: result[0].length,
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+
+app.get("/most-expensive-order", async (req, res) => {
+  try {
+    const result = await mostExpensiveOrder();
+    res.status(200).json({
+      status: "success",
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+
+app.get("/least-expensive-order", async (req, res) => {
+  try {
+    const result = await leastExpensiveOrder();
+    res.status(200).json({
+      status: "success",
+      orders: result[0],
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+});
+
 app.use(/^\/products?/, productRouter);
 app.use(/^\/orders?/, orderRouter);
 
 export default app;
+
+mostExpensiveOrder;
--- Documents/pratice-database/mysql/database.js
+++ Documents/pratice-database/mysql/database.js
@@ -90,5 +90,50 @@ export async function deleteOrder(id) {
   return result;
 }
 
-("select u.name , o.id as order_id , p.title as product_name , order_status , delivery_date  from order_details od join products p on od.product_id = p.id join orders o on od.order_id = o.id join user u on o.user_id = u.id");
-("select u.name , o.id as order_id , p.title as product_name , order_status , delivery_date  from order_details od join products p on od.product_id = p.id join orders o on od.order_id = o.id join user u on o.user_id = u.id");
+export async function getAllUndeliverOrder() {
+  const result = await pool.query(
+    "SELECT u.name , o.id as order_id , p.title as product_name , order_status , delivery_date  from order_details od JOIN products p ON od.product_id = p.id JOIN orders o ON od.order_id = o.id JOIN user u ON o.user_id = u.id WHERE order_status = 'pending'"
+  );
+  return result;
+}
+export async function fiveRecentOrder() {
+  const result = await pool.query(
+    "SELECT u.name , o.id as order_id , p.title as product_name , order_status, order_date , delivery_date  from order_details od JOIN products p ON od.product_id = p.id JOIN orders o ON od.order_id = o.id JOIN user u ON o.user_id = u.id ORDER BY order_date DESC LIMIT 5"
+  );
+  return result;
+}
+
+export async function topActiveUser() {
+  const result = await pool.query(
+    "SELECT u.name ,count(u.id) as 'Total Orders' from orders o JOIN user u ON o.user_id = u.id group by u.id order by count(u.id) desc limit 5"
+  );
+  return result;
+}
+
+export async function inActiveUser() {
+  const result = await pool.query(
+    "SELECT u.id as 'User Id',name , email , address  FROM user u  LEFT JOIN orders o ON u.id = o.user_id WHERE o.id IS NULL;"
+  );
+  return result;
+}
+
+export async function mostPurchasedProduct() {
+  const result = await pool.query(
+    "SELECT product_id, COUNT(product_id) AS 'Total Purchase', title, description, amount FROM order_details od  join products p  ON od.product_id = p.id GROUP BY product_id ORDER BY COUNT(product_ID) DESC LIMIT 5"
+  );
+  return result;
+}
+
+export async function mostExpensiveOrder() {
+  const result = await pool.query(
+    "SELECT od.order_id, SUM(p.amount) AS total_amount, GROUP_CONCAT(p.title SEPARATOR ', ') AS product_purchased FROM order_details od JOIN products p ON od.product_id = p.id GROUP BY od.order_id order by total_amount desc limit 1;"
+  );
+  return result;
+}
+
+export async function leastExpensiveOrder() {
+  const result = await pool.query(
+    "SELECT od.order_id, SUM(p.amount) AS total_amount, GROUP_CONCAT(p.title SEPARATOR ', ') AS product_purchased FROM order_details od JOIN products p ON od.product_id = p.id GROUP BY od.order_id order by total_amount limit 1;"
+  );
+  return result;
+}
